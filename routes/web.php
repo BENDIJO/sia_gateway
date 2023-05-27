857d055f6a5ed2a667f6c2f6d4257da92223869c
rs183c56 <b.ricx14@gmail.com>
2023-05-27 08:53:38 +0800

    modified

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'v1'], function($router) {
+    $router->get('/users', 'site1Controller@showUsers');
+
+    $router->get('/user/{id}', 'site1Controller@showUser');
+
+    $router->delete('/user/{id}', 'site1Controller@deleteUser');
+
+    $router->post('/user', 'site1Controller@createUser');
+
+    $router->patch('/user/{id}', 'site1Controller@patchUser');
+
+});
+
+
+$router->group(['prefix' => 'v2'], function($router) {
+    $router->get('/users', 'site2Controller@showUsers');
+
+    $router->get('/user/{id}', 'site2Controller@showUser');
+
+    $router->delete('/user/{id}', 'site2Controller@deleteUser');
+
+    $router->post('/user', 'site2Controller@createUser');
+
+    $router->patch('/user/{id}', 'site2Controller@patchUser');
+
+});
